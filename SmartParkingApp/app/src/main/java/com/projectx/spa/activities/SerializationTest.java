835c81567edf2146c1c1a682dbe044701b4e5cad
Raghuvorkady <raghavendrakm300@gmail.com>
2021-06-30 13:45:26 +0530

    renamed activity_test.xml

--- SmartParkingApp/app/src/main/java/com/projectx/spa/activities/SerializationTest.java
+++ SmartParkingApp/app/src/main/java/com/projectx/spa/activities/SerializationTest.java
@@ -17,7 +17,7 @@ public class SerializationTest extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_serialization_test);
 
         TextView textView = findViewById(R.id.text);
 
