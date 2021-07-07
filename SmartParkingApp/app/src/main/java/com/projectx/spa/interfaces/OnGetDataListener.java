36d06d5dd551f1a8eae3028d4175b223787e7ab6
Raghuvorkady <raghavendrakm300@gmail.com>
2021-07-07 18:36:13 +0530

    added interface OnGetDataListener

---
+++ SmartParkingApp/app/src/main/java/com/projectx/spa/interfaces/OnGetDataListener.java
@@ -0,0 +1,12 @@
+package com.projectx.spa.interfaces;
+
+import com.google.firebase.firestore.DocumentReference;
+
+public interface OnGetDataListener {
+    // if data gets added successfully
+    void onSuccess(DocumentReference dataSnapshotValue);
+
+    // if data does not get added
+    void onFailure(String str);
+
+}
