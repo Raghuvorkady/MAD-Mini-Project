cb5dabdf0c5832e0bd079e8c5d79cc3c8c2e687f
Raghuvorkady <raghavendrakm300@gmail.com>
2021-07-11 00:41:02 +0530

    refactored DetailsActivity

--- SmartParkingApp/app/src/main/java/com/projectx/spa/activities/DetailsActivity.java
+++ SmartParkingApp/app/src/main/java/com/projectx/spa/activities/DetailsActivity.java
@@ -1,6 +1,5 @@
 package com.projectx.spa.activities;
 
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +7,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
@@ -29,48 +30,60 @@ import com.santalu.maskara.widget.MaskEditText;
 public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = getClass().getSimpleName();
 
-    CardView in;
-    ProgressBar wait;
-    MaskEditText maskEditText;
-    String vehicleNumber;
-    String availableSpace, id;
-    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private CardView in;
+    private ProgressBar wait;
+    private MaskEditText maskEditText;
+
+    private String vehicleNumber;
+    private int availableSpace;
+    private String id;
+
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        wait=findViewById(R.id.waiting);
-        in=findViewById(R.id.in_card);
+        wait = findViewById(R.id.waiting);
+        in = findViewById(R.id.in_card);
+        maskEditText = findViewById(R.id.vehicle_edit_text);
+
         in.setOnClickListener(this);
-        Intent intent = getIntent();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
         id = new UserSession(this).getUserDetails().get(Constants.PREF_ID);
         Log.d(TAG, id);
-        // Log is recommended
 
         DocumentReference docRef = firebaseFirestore.collection(Constants.PARKING_SLOTS).document(id);
-        docRef.get()
+        docRef
+                .get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        availableSpace = documentSnapshot.get("availableSpace").toString();
+                        availableSpace = Integer.parseInt(documentSnapshot.get("availableSpace").toString());
                         Log.d(TAG, "avail=" + availableSpace);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "failed " + e.getMessage());
                     }
                 });
 
-        maskEditText = findViewById(R.id.vehicle_edit_text);
         /*e1.setFilters(new InputFilter[] {new InputFilter.AllCaps()});*/
     }
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public void onClick(View view) {
-        if (view.equals(in)){
-            if (Integer.parseInt(availableSpace) > 0) {
+        if (view.equals(in)) {
+            if (availableSpace > 0) {
                 vehicleNumber = maskEditText.getText().toString();
                 if (!vehicleNumber.equals("AA-00-BB-1111")) {
-                    System.out.println(vehicleNumber);
+                    Log.d(TAG, vehicleNumber);
                     // Intent it = new Intent(this, AdminHomeActivity.class);
                     if ((vehicleNumber.matches("^[A-Z]{2}[-][0-9]{2}[-][A-Z]{2}[-][0-9]{4}$"))) {
                         maskEditText.setText("");
@@ -79,33 +92,50 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
                                 .setTitle("Insert entry")
                                 .setMessage("Are you sure you want to insert " + vehicleNumber + "?")
                                 .setPositiveButton(android.R.string.yes, (dialog, which) -> {
-
                                     in.setVisibility(View.INVISIBLE);
                                     wait.setVisibility(View.VISIBLE);
-                                    ParkedVehicle parkedVehicle = new ParkedVehicle(null, vehicleNumber, Timestamp.now());
-                                FbHelper fbHelper = new FbHelper(getApplicationContext());
-                                String collectionReference = Constants.PARKING_SLOTS + "/" + id + "/" + Constants.PARKED_VEHICLES;
-                                fbHelper.addDataToFirestore(parkedVehicle, collectionReference, null, new OnGetDataListener() {
-                                        @Override
-                                        public void onSuccess(DocumentReference dataSnapshotValue) {
-                                            Toast.makeText(DetailsActivity.this, "added successfully", Toast.LENGTH_LONG).show();
-                                            int updatedAvailableSpace = Integer.parseInt(availableSpace) - 1;
-                                            firebaseFirestore.collection(Constants.PARKING_SLOTS).document(id).update("availableSpace", updatedAvailableSpace);
-                                            // startActivity(it);
-                                            finish();
-                                        }
 
-                                        @Override
-                                        public void onFailure(String str) {
-                                            Log.w(TAG, "Error adding document " + str);
-                                        }
-                                    });
+                                    ParkedVehicle parkedVehicle = new ParkedVehicle(null, vehicleNumber, Timestamp.now());
+
+                                    FbHelper fbHelper = new FbHelper(getApplicationContext());
+                                    String collectionReference = Constants.PARKING_SLOTS + "/" + id + "/" + Constants.PARKED_VEHICLES;
+
+                                    fbHelper.addDataToFirestore(parkedVehicle, collectionReference, null,
+                                            new OnGetDataListener() {
+                                                @Override
+                                                public void onSuccess(DocumentReference dataSnapshotValue) {
+                                                    Toast.makeText(DetailsActivity.this, "added successfully", Toast.LENGTH_LONG).show();
+                                                    int updatedAvailableSpace = availableSpace - 1;
+
+                                                    firebaseFirestore.collection(Constants.PARKING_SLOTS)
+                                                            .document(id)
+                                                            .update("availableSpace", updatedAvailableSpace)
+                                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                @Override
+                                                                public void onSuccess(Void unused) {
+                                                                    Log.d(TAG, "success");
+                                                                }
+                                                            }).addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull Exception e) {
+                                                            Log.d(TAG, "failed " + e.getMessage());
+                                                        }
+                                                    });
+                                                    // startActivity(it);
+                                                    finish();
+                                                }
+
+                                                @Override
+                                                public void onFailure(String str) {
+                                                    Log.w(TAG, "Error adding document " + str);
+                                                }
+                                            });
                                 })
                                 .setNegativeButton(android.R.string.no, null)
                                 .show();
                     } else {
                         Log.d(TAG, "wrong");
-                        show("wrong format");
+                        makeToast("wrong format");
                     }
                 }
             } else {
@@ -114,7 +144,7 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    private void show(String s) {
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    private void makeToast(String toastMessage) {
+        Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
     }
 }
