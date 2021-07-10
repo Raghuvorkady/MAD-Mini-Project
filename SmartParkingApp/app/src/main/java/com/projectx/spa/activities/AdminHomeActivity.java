6d4a0ec945c44bc8b9175722dc26c9b951ff536c
Raghuvorkady <raghavendrakm300@gmail.com>
2021-07-11 00:33:23 +0530

    refactored AdminHomeActivity

--- SmartParkingApp/app/src/main/java/com/projectx/spa/activities/AdminHomeActivity.java
+++ SmartParkingApp/app/src/main/java/com/projectx/spa/activities/AdminHomeActivity.java
@@ -11,6 +11,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -23,62 +24,68 @@ import com.projectx.spa.helpers.Constants;
 import com.projectx.spa.helpers.UserSession;
 import com.projectx.spa.models.ParkingSlot;
 
-import androidx.cardview.widget.CardView;
 import es.dmoral.toasty.Toasty;
 
-public class AdminHomeActivity extends AppCompatActivity implements View.OnClickListener{
-
-    TextView nameTextView, buildingTextView, landTextView, availableTextView;
-    CardView parkedVehicle,history;
-    int availableSpace;
-    String id;
+public class AdminHomeActivity extends AppCompatActivity implements View.OnClickListener {
     private final String TAG = getClass().getSimpleName();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    UserSession userSession;
-    ProgressBar progress;
+
+    private TextView nameTextView;
+    private TextView buildingTextView;
+    private TextView landTextView;
+    private TextView availableTextView;
+    private CardView parkedVehicleCardView, historyCardView;
+    private int availableSpace;
+    private UserSession userSession;
+    private ProgressBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
-        // user = new User("VpnN3ycQOyRjg5vpqPoO6ag5lBb2", "testName", "testMail", "1213213", Timestamp.now());
 
         nameTextView = findViewById(R.id.name_text_view);
         buildingTextView = findViewById(R.id.building_text_view);
         landTextView = findViewById(R.id.land_text_view);
         availableTextView = findViewById(R.id.linear_layout1_available_slots);
-        parkedVehicle=findViewById(R.id.contraint_layout3_info_card);
-        history=findViewById(R.id.out_card);
-        progress=findViewById(R.id.loading);
+        parkedVehicleCardView = findViewById(R.id.contraint_layout3_info_card);
+        historyCardView = findViewById(R.id.history_card);
+        progress = findViewById(R.id.loading);
 
-        parkedVehicle.setOnClickListener(this);
-        history.setOnClickListener(this);
+        parkedVehicleCardView.setOnClickListener(this);
+        historyCardView.setOnClickListener(this);
 
         userSession = new UserSession(this);
-        id = userSession.getUserDetails().get(Constants.PREF_ID);
-        DocumentReference documentReference = FirebaseFirestore.getInstance().collection(Constants.PARKING_SLOTS).document(id);
-        trackSingleDocumentTest(documentReference);
+        String id = userSession.getUserDetails().get(Constants.PREF_ID);
+        DocumentReference documentReference;
+        if (id != null) {
+            documentReference = FirebaseFirestore.getInstance().collection(Constants.PARKING_SLOTS).document(id);
+            trackSingleDocumentTest(documentReference);
+        } else {
+            Log.d(TAG, "id is null");
+        }
     }
 
     @Override
     public void onClick(View view) {
-        parkedVehicle.setVisibility(View.INVISIBLE);
-        history.setVisibility(View.INVISIBLE);
+        parkedVehicleCardView.setVisibility(View.INVISIBLE);
+        historyCardView.setVisibility(View.INVISIBLE);
         progress.setVisibility(View.VISIBLE);
 
-        Intent intent=new Intent();
-        if (view.equals(parkedVehicle)){
+        Intent intent = new Intent();
+        if (view.equals(parkedVehicleCardView)) {
             intent.setClass(this, ParkedVehiclesActivity.class);
             startActivity(intent);
         }
-        if (view.equals(history)){
+        if (view.equals(historyCardView)) {
             intent.setClass(this, ParkedHistoryActivity.class);
             startActivity(intent);
         }
-        parkedVehicle.setVisibility(View.VISIBLE);
-        history.setVisibility(View.VISIBLE);
+
+        parkedVehicleCardView.setVisibility(View.VISIBLE);
+        historyCardView.setVisibility(View.VISIBLE);
         progress.setVisibility(View.INVISIBLE);
     }
+
     private void trackSingleDocumentTest(DocumentReference documentReference) {
         documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
             @Override
@@ -110,17 +117,6 @@ public class AdminHomeActivity extends AppCompatActivity implements View.OnClick
         });
     }
 
-    public void historyPage(View view) {
-        Intent intent = new Intent(this, ParkedHistoryActivity.class);
-        startActivity(intent);
-    }
-
-    public void viewParkedVehicles(View view) {
-        Log.d(TAG, "ADMIN vehicles");
-        Intent intent = new Intent(this, ParkedVehiclesActivity.class);
-        startActivity(intent);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -132,8 +128,8 @@ public class AdminHomeActivity extends AppCompatActivity implements View.OnClick
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
         switch (item.getItemId()) {
-           //case R.id.menu_profile:
-           //     profilePage();
+            //case R.id.menu_profile:
+            //     profilePage();
             //    return true;
             case R.id.menu_logout:
                 logOut();
@@ -152,5 +148,5 @@ public class AdminHomeActivity extends AppCompatActivity implements View.OnClick
 
     //private void profilePage() {
     //    Log.d(TAG, "yet to implement");
-   // }
+    // }
 }
